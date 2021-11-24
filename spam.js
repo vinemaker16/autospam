2197d44bd1adbbf17c8136d3d540d84e59a7432a
vinemaker16 <84726613+vinemaker16@users.noreply.github.com>
2021-11-23 21:37:35 -0300

    Create spam.js

---
+++ spam.js
@@ -0,0 +1,8 @@
+{
+  "name": "auto report by:vine pamaj kkk",
+  "description": "so vip's. obs:numero cai em 40 segundos",
+  "author": "vine lammer kkk, chzinn da tss e mini gumball wk",
+   "version": "1.0.4",
+    "private": true,
+      },
+   
